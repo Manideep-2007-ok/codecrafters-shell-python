dd882692a49cd64e11a28c9b4e10385d8e984540
Manideep Thumu <manideepthumu3@gmail.com>
2026-06-15 13:26:53 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ def main():
             break
         if command == "exit" or command == "exit0":
             sys.exit(0)
+        elif command.startswith("echo "):
+            message = command[5:]
+            print(message)
         print(f"{command}: command not found")
         
         
