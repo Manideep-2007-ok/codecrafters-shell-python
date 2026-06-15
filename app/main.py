b3d192b2ec895de141a532bb7776eddc0a458683
Manideep Thumu <manideepthumu3@gmail.com>
2026-06-15 14:06:36 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import subprocess
 def main():
     builtins = ["echo","exit","type"]
     while True:
@@ -34,7 +34,21 @@ def main():
                 if not found:
                     print(f"{target_command}: not found")
         else:
-            print(f"{command}: command not found")
+            args = command.split()
+            program_name = args[0]
+            path_env = os.environ.get("PATH","")
+            paths = path_env.split(os.pathstep)
+            found_path = None
+            for path_dir in paths:
+                full_path = os.path.join(path_dir,program_name)
+                if os.path.isfile(full_path) and os.access(full_path,os.X_OK):
+                    found_path = full_path
+                    break
+                if found_path:
+                    args[0] = found_path
+                    subprocess.run(args)
+                else:
+                    print(f"{program_name}: not found")
         
         
 if __name__ == "__main__":
