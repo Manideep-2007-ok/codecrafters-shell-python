bc017cc1d1dbb91a28e38a22e7dbb0ecc42afee6
Manideep Thumu <manideepthumu3@gmail.com>
2026-06-15 16:22:01 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -47,11 +47,23 @@ def main():
         args = parse_arguments(command)
         if not args:
             continue
+        redirect_file = None
+        if ">" in args:
+            idx = args.index(">")
+            redirect_file = args[idx+1]
+            args = args[:idx]+args[idx+2:]
+        if "1>" in args:
+            idx = args.index("1>")
+            redirect_file = args[idx+1]
+            args = args[:idx]+args[idx+2:]
+        out_fp = open(redirect_file,"w") if redirect_file else sys.stdout
         cmd = args[0]
         if cmd == "exit" or cmd == "exit 0":
+            if redirect_file:
+                out_fp.close()
             sys.exit(0)
         elif cmd=="echo":
-            print(" ".join(args[1:]))
+            print(" ".join(args[1:]), file=out_fp)
         elif cmd=="type":
             if len(args)>1:
                 target_command = args[1]
@@ -91,10 +103,11 @@ def main():
                     found_path = full_path
                     break
             if found_path:
-                subprocess.run(args, executable=found_path)
+                subprocess.run(args, executable=found_path, stdout = out_fp)
             else:
                 print(f"{program_name}: command not found")
-        
+        if redirect_file:
+            out_fp.close()
         
 if __name__ == "__main__":
     main()
