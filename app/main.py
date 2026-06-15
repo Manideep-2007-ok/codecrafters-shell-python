3682f954e659ee6acdbc2dbf03b4e243407fd8c9
Manideep Thumu <manideepthumu3@gmail.com>
2026-06-15 13:36:26 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtins = ["echo","exit","type"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -14,6 +15,12 @@ def main():
         elif command.startswith("echo "):
             message = command[5:]
             print(message)
+        elif command.startswith("type "):
+            target_command = command[5:]
+            if target_command in builtins:
+                print(f"{target_command} is a shell builtin")
+            else:
+                print(f"{target_command} not found")
         else:
             print(f"{command}: command not found")
         
